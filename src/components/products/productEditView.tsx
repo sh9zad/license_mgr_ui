8b6563c94e5ed6e5866a2074b3bfa6a8326baffe
Shervin <shassanzadeh@videoamp.com>
2018-09-14 16:23:50 -0700

    Edit view is updated

--- src/components/products/productEditView.tsx
+++ src/components/products/productEditView.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { IProduct } from "../../models/Product";
 import { RouteComponentProps } from "react-router";
+import { AlertDlg } from "../stateless/alert";
 
 interface IState {
   product: IProduct;
+  showAlert: boolean;
+  alertMessage: string;
+  alertType: string;
 }
 
 export class ProductEditView extends React.Component<
@@ -11,7 +15,10 @@ export class ProductEditView extends React.Component<
   IState
 > {
   public state: IState = {
-    product: {}
+    alertMessage: "",
+    alertType: "",
+    product: {},
+    showAlert: false
   };
 
   public componentDidMount(): void {
@@ -23,10 +30,106 @@ export class ProductEditView extends React.Component<
       .then((data: IProduct) => this.setState({ product: data }));
   }
 
+  public closeAlertDialog() {
+    this.setState({ showAlert: false, alertMessage: "" });
+  }
+
+  public inputChangeHandler(e: React.FormEvent<HTMLInputElement>): void {
+    const { product } = this.state;
+    product[e.currentTarget.id] = e.currentTarget.value;
+    this.setState({ product });
+  }
+
+  public formSubmitHandler() {
+    const { product } = this.state;
+    //tslint:disable
+    fetch("http://localhost:3030/product/" + this.state.product._id, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.errors) {
+          this.setState({
+            alertType: "error",
+            alertMessage: data.message,
+            showAlert: true
+          });
+        } else {
+          this.setState({
+            alertType: "success",
+            alertMessage: "Product added successfully",
+            showAlert: true,
+            product: data
+          });
+        }
+      })
+      .catch((reason: any) => {
+        console.log("here");
+        this.setState({
+          alertMessage: reason,
+          showAlert: true
+        });
+      });
+  }
+
+  public onInputChange = (e: React.FormEvent<HTMLInputElement>): void =>
+    this.inputChangeHandler(e);
+  public onFormSubmit = (): void => this.formSubmitHandler();
+  public onCloseDialog = (): void => this.closeAlertDialog();
+
   public render() {
+    const { product } = this.state;
     return (
       <div>
-        <h3>EditView</h3>
+        <AlertDlg
+          level={this.state.alertType}
+          message={this.state.alertMessage}
+          visible={this.state.showAlert}
+          closeDialog={this.onCloseDialog}
+        />
+        <div className={"row  justify-content-md-center"}>
+          <h3 className={"col-sm-10"}>Add New Product</h3>
+        </div>
+        <div className={"row justify-content-md-center"}>
+          <div className={"col-sm-9"}>
+            <label>Name</label>
+            <input
+              className="form-control"
+              id="name"
+              type="text"
+              value={product.name}
+              onChange={this.onInputChange}
+            />
+            <label>Code</label>
+            <input
+              className="form-control"
+              id="code"
+              type="text"
+              value={product.code}
+              onChange={this.onInputChange}
+            />
+            <label>Salt</label>
+            <input
+              className="form-control"
+              id="salt"
+              type="text"
+              value={product.salt}
+              onChange={this.onInputChange}
+            />
+            <button
+              className="btn btn-success btn-small"
+              type="button"
+              onClick={this.onFormSubmit}
+            >
+              <span className="fa fa-edit" /> Edit
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
